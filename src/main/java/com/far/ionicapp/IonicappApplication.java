9e61f2a3ff91ad2cfff3ed40dfa41cf33c1c73a5
Felipe Almeida <felipe@2far.com.br>
2019-01-26 16:05:48 -0200

    Estrutura de projeto

---
+++ src/main/java/com/far/ionicapp/IonicappApplication.java
@@ -0,0 +1,14 @@
+package com.far.ionicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonicappApplication.class, args);
+	}
+
+}
+
