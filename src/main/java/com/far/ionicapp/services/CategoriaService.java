deda7d0258d702a1ece5746ccc5a0493c49eaf26
Felipe Almeida <felipe@2far.com.br>
2019-01-27 22:30:56 -0200

    Paginacao com Parametros Opcionais na Request

--- src/main/java/com/far/ionicapp/services/CategoriaService.java
+++ src/main/java/com/far/ionicapp/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.far.ionicapp.domain.Categoria;
@@ -39,6 +42,11 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		repository.deleteById(id);
+	}
+
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest) ;
 	}
 
 }
