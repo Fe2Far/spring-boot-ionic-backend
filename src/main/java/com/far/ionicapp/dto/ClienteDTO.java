d0267b4ea8813df137c8136898488b0623bc2057
Felipe Almeida <felipe@2far.com.br>
2019-01-29 14:31:45 -0200

    Validação email já existente na atualização de cliente

--- src/main/java/com/far/ionicapp/dto/ClienteDTO.java
+++ src/main/java/com/far/ionicapp/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.far.ionicapp.domain.Cliente;
+import com.far.ionicapp.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
