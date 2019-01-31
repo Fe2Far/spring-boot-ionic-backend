c4e38f303486850c2063e393ca0af826a3893495
Felipe Almeida <felipe@2far.com.br>
2019-01-30 22:13:52 -0200

    Profile de desenvolvimento

--- src/main/java/com/far/ionicapp/config/TestConfig.java
+++ src/main/java/com/far/ionicapp/config/TestConfig.java
@@ -3,10 +3,12 @@ package com.far.ionicapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.far.ionicapp.services.DBService;
 
 @Configuration
+@Profile("test")
 public class TestConfig {
 	
 	@Autowired
