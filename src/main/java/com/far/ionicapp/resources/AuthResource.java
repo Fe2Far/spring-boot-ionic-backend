d34af2a6bf24bb2068e949510c0501951215d8af
Felipe Almeida <felipe@2far.com.br>
2019-02-03 17:36:49 -0200

    Refresh Token
    Signed-off-by: Felipe Almeida <felipe@2far.com.br>

---
+++ src/main/java/com/far/ionicapp/resources/AuthResource.java
@@ -0,0 +1,27 @@
+package com.far.ionicapp.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.far.ionicapp.security.JWTUtil;
+import com.far.ionicapp.security.UserSS;
+import com.far.ionicapp.security.UserService;
+
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
